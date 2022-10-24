dd0a42500ac0895c815d1f4c2ed55c96577f0b1b
Aiden Thomas <aiden@devpipeline.com>
2022-10-23 20:39:53 -0600

    added the fourth task

---
+++ assignment-4.js
@@ -0,0 +1,25 @@
+function alphabeticalLetters(string) {
+  let splitString = string.split(" ");
+  let combinedString = "";
+  let lettersArray = [];
+  let finalString = "";
+
+  for (word in splitString) {
+    combinedString += splitString[word];
+  }
+
+  for (letter in combinedString) {
+    lettersArray.push(combinedString[letter]);
+  }
+
+  lettersArray.sort();
+
+  for (letter in lettersArray) {
+    finalString += lettersArray[letter];
+  }
+
+  return finalString;
+}
+
+let inputString = "Something in the way";
+console.log(alphabeticalLetters(inputString));
